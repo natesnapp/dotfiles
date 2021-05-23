d37f24e96b25f3fd5b0140b299fcf7a5996ffed5
Moshe Zadka <zadka.moshe@gmail.com>
2021-05-23 09:15:13 -0700

    move to pyver

--- venvwrapper.bash
+++ venvwrapper.bash
@@ -1,4 +1,6 @@
+export PATH=$PATH:~/bin
 export WORKON_HOME=$HOME/.virtualenvs
-PYENV_PY3_LOCATION=$(dirname $(pyenv which python3))
-export PATH=$PATH:$PYENV_PY3_LOCATION
-source $PYENV_PY3_LOCATION/virtualenvwrapper.sh
+export PYVER_ROOT=$HOME/.pyver
+export PYVER_PY3_LOCATION=$(dirname $(pyver which 3.9))
+export PATH=$PYVER_PY3_LOCATION:$PATH
+source $PYVER_PY3_LOCATION/virtualenvwrapper.sh
