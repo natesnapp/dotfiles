c08fa959515d10daee6eb9d3ddab4f95b9453bf5
Moshe Zadka <moshez@zadka.club>
2022-01-02 16:45:45 +0000

    keep issues separate -- this should assume the right python is running

--- venvwrapper.bash
+++ venvwrapper.bash
@@ -1,6 +1,2 @@
-export PATH=$PATH:~/bin
-export WORKON_HOME=$HOME/.virtualenvs
-export PYVER_ROOT=$HOME/.pyver
-export PYVER_PY3_LOCATION=$(dirname $(pyver which 3.9))
-export PATH=$PYVER_PY3_LOCATION:$PATH
-source $PYVER_PY3_LOCATION/virtualenvwrapper.sh
+VENVWRAPPER="$(python -c 'import sys;print(sys.prefix)')/bin/virtualenvwrapper.sh"
+[ -f $VENVWRAPPER ] && source $VENVWRAPPER
