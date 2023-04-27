a66fdc66110909223e87adcb7f55fb4cbbae98e3
Nate Snapp <nate.snapp@gmail.com>
2023-04-26 21:08:41 -0600

    switch bashrc to bash_profile

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DOTFILES="$(dirname $(realpath $0))"
 [ -L ~/.config ] || ln -s $DOTFILES/config ~/.config
-grep -q DOTFILES ~/.bashrc || echo "export DOTFILES=$DOTFILES" >> ~/.bashrc
+grep -q DOTFILES ~/.bash_profile || echo "export DOTFILES=$DOTFILES" >> ~/.bash_profile
 for script in starship venvwrapper ssh local-bin;
 do
-grep -q $script ~/.bashrc || echo "source ~/src/dotfiles/$script.bash" >> ~/.bashrc
+grep -q $script ~/.bash_profile || echo "source ~/src/dotfiles/$script.bash" >> ~/.bash_profile
 done
 python -m pip install --user pipx
